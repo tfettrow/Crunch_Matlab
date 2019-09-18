1ecc6eb6cefae5bd4eff4dd0f2ecd5f4d1539e89
taysgd <53057107+taysgd@users.noreply.github.com>
2019-09-18 14:42:39 -0400

    Cleaned up slice_time

--- scripts/slicetime_fmri.m
+++ scripts/slicetime_fmri.m
@@ -10,9 +10,10 @@ spm('Defaults','fMRI');
 spm_jobman('initcfg');
 spm_get_defaults('cmdline',true);
 
-json_path_names =spm_select('FPList', data_path, '^fMRI.*\.json$');
+% TO DO: Fix the file naming convention
+json_path_names =spm_select('FPList', data_path, 'fMRI.*\.json$');
 
-all_files_to_slicetime_volumes = spm_select('ExtFPList', data_path, '^fMRI.*\.nii$');
+all_files_to_slicetime_volumes = spm_select('ExtFPList', data_path, 'fMRI.*\.nii$');
 
 for i_file = 1 : size(all_files_to_slicetime_volumes,1)
     string_files_to_slicetime_volumes(i_file, :) = convertCharsToStrings(all_files_to_slicetime_volumes(i_file, :));
@@ -32,21 +33,8 @@ matlabbatch{1}.spm.temporal.st.scans = all_scans_fullpath_scan_cell;
 [stMsec, TRsec] = bidsSliceTiming(json_path_names(1,:));
 if isempty(stMsec) || isempty(TRsec), error('Update dcm2niix? Unable determine slice timeing or TR from BIDS file %s', BIDSname); end;
 
-unique_slicetimes = unique(stMsec);
-if length(unique_slicetimes) == length(stMsec)/3
-    % multiband by 3s (UF Siemens 3T)
-    refslice = median(stMsec);
-    if ~any(stMsec == refslice)
-        difference_from_median = (stMsec - refslice);
-        refslice = min(stMsec(difference_from_median > 0));
-    end
-elseif length(unique_slicetimes) == length(stMsec)
-    % ascending or descending
-    refslice = max(stMsec)/2;
-    fprintf('Setting reference slice as %g ms (slice ordering assumed to be from foot to head)\n', refslice);
-else
-    error('Something is not right with slice order, check whether multiband not equal to 3')
-end
+refslice = max(stMsec)/2;
+fprintf('Setting reference slice as %g ms (slice ordering assumed to be from foot to head)\n', refslice);
 
 timing = [0 TRsec];
 matlabbatch{1}.spm.temporal.st.nslices = length(stMsec);
