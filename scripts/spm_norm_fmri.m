12bd897a85428330698001e923d38d3126f9e821
tfettrow <tfettrow@gmail.com>
2019-09-23 18:44:50 -0400

    slight change to segmentation and normalization. we use the deformation field from segmentation to apply during normalization write (not estimate and write). coregistration of t1 to fmri is necessary bc we use the deformation field of t1 segmentation.

--- scripts/spm_norm_fmri.m
+++ scripts/spm_norm_fmri.m
@@ -19,42 +19,54 @@ end
 for i_file = 1 : size(files_to_normalise,1)
     this_file_with_volumes = spm_select('expand', files_to_normalise(i_file,:));
     
-    matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = cellstr(subject_specific_template);
-    matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = cellstr(this_file_with_volumes);
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'TPM.nii'};
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
-        78 76 85];
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [2 2 2];
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = 'normalized2t1_';
-
-
+    matlabbatch{1}.spm.spatial.normalise.write.subj.def  = cellstr('y_T1.nii');
+    matlabbatch{1}.spm.spatial.normalise.write.subj.resample = cellstr(this_file_with_volumes);
+    
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.vox  = [3 3 3];
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'normalized_';
+ 
     spm_jobman('run',matlabbatch);
     clear matlabbatch
+    
+    
+    
+   % DO NOT ESTIMATE AND WRITE 
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = cellstr(subject_specific_template);
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = cellstr(this_file_with_volumes);
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'TPM.nii'};
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
+%         78 76 85];
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [0.8 0.8 0.8];
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = 'normalized2t1_';
+% 
+% 
+%     spm_jobman('run',matlabbatch);
+%     clear matlabbatch
    
     
-    matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = cellstr(this_template);
-    matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = cellstr(this_file_with_volumes);
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'TPM.nii'};
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
-        78 76 85];
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [2 2 2];
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
-    matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = 'normalized2MNI_';
-
-
-    spm_jobman('run',matlabbatch);
-    clear matlabbatch
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = cellstr(this_template);
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = cellstr(this_file_with_volumes);
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'TPM.nii'};
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
+%         78 76 85];
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [2 2 2];
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
+%     matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = 'normalized2MNI_';
+% 
+% 
+%     spm_jobman('run',matlabbatch);
+%     clear matlabbatch
 end
