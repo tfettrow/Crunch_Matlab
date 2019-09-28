b93a117985f79bbacddb619bc97fc6e521a37afa
tfettrow <tfettrow@gmail.com>
2019-09-28 09:53:40 -0400

    new art function allows appropriate renaming of resulting files

--- scripts/art_fmri.m
+++ scripts/art_fmri.m
@@ -1,26 +1,29 @@
-
+function art_fmri(functional_file_name)
 data_path = pwd;
 
-clear matlabbatch
-spm('Defaults','fMRI');
-spm_jobman('initcfg');
-spm_get_defaults('cmdline',true);
+    clear matlabbatch
+    spm('Defaults','fMRI');
+    spm_jobman('initcfg');
+    spm_get_defaults('cmdline',true);
 
 
-all_files_to_art = spm_select('FPList', data_path, '^unwarpedRealigned_slicetimed_.*\.nii$');
-
-for i_file = 1 : size(all_files_to_art,1)
-    this_file_to_art = all_files_to_art(i_file,:);
+    this_file_to_art = spm_select('FPList', data_path, strcat('^',functional_file_name,'$'));
+    this_structural = spm_select('FPList', data_path, '^T1.*\.nii$');
     
-    BATCH.New.structurals{1} = 'T1.nii';
+    BATCH.New.structurals{1} = this_structural;
     
     BATCH.New.functionals{1}{1} = this_file_to_art;
     BATCH.New.steps = {'functional_art'};
     BATCH.filename = 'Conn_Art_Folder_Stuff';
-    BATCH.New.art_thresholds(1)= 9;
-    BATCH.New.art_thresholds(2)= 2;
+    BATCH.New.art_thresholds(1)= 9;  % z threshold
+    BATCH.New.art_thresholds(2)= 2;  % movement threshold
     
     conn_batch(BATCH)
-end
-   
+    
+    
+    [filepath,file_name,ext] = fileparts(this_file_to_art);
+    
+    movefile('art_screenshot.jpg', strcat('ARTscreenshot_',file_name,'.jpg'))
 
+  
+end
