9e23b7584bc3756247115d65c697e491edd9a715
tfettrow <tfettrow@gmail.com>
2019-10-19 10:23:18 -0400

    level one outlier removal model adjustement a gooo

--- scripts/level_one_stats.m
+++ scripts/level_one_stats.m
@@ -1,20 +1,5 @@
 function level_one_stats(TR_from_json)
-
-    % Things to fix
-    % 1) Need to ensure the duration and onset times end/start on a TR
-%     2) need to adjust condition onsets in case of outlier removals (these
-%     are with respect to first volume acquired (1 volume or 0sec)
-    % 3) i_file is used way too many times !!!!
     
-%    outlier removal automation
-%  1) determine which indices were removed for this run (have bash pass it)
-%  2) determine the time to be removed (TR * volumes)
-%  3) subtract all onset times by time to be removed if at beginning of run
-%  3a) first condition will be negative (start at 0 and duration should be
-%  taken care of (see line 59ish))
-%  4) when is duration too short to include in analysis?? 
-    
-% 
     data_path = pwd;
     clear matlabbatch
     spm('Defaults','fMRI');
@@ -23,11 +8,61 @@ function level_one_stats(TR_from_json)
 
     files_to_test = spm_select('FPList', data_path, '^smoothed.*\.nii$');
     art_regression_outlier_files  = spm_select('FPList',  data_path,'^art_regression_outliers_and_movement.*.mat');
-
-    condition_onset_structs = dir('Condition_Onsets*.*');
-    for i_run = 1:size(condition_onset_structs,1)
-        condition_onset_files(i_run,:) = fullfile(condition_onset_structs(i_run).folder,condition_onset_structs(i_run).name);
+    condition_onset_files = spm_select('FPList', data_path, '^Condition_Onsets_.*.csv');
+    
+    directory_pieces = regexp(data_path,'\','split');
+    subject_level_directory = getfield( directory_pieces, {1:length(directory_pieces)-3} );
+    for i_path_element = 1:size(subject_level_directory,2);
+        subject_level_directory{i_path_element}(end+1) = '/';
     end
+    subject_level_directory_string = cellfun(@string,subject_level_directory);
+    subject_level_directory_full = join(subject_level_directory_string,'');
+    
+    outlier_settings_file_path = fullfile(subject_level_directory_full, 'outlier_removal_settings.txt');
+    
+     fileID = fopen(outlier_settings_file_path, 'r');
+    
+    % read text to cell
+    text_line = fgetl(fileID);
+    text_cell = {};
+    while ischar(text_line)
+        text_cell = [text_cell; text_line]; %#ok<AGROW>
+        text_line = fgetl(fileID);
+    end
+    fclose(fileID);
+    
+     % prune lines
+     lines_to_prune = false(size(text_cell, 1), 1);
+     for i_line = 1 : size(text_cell, 1)
+         this_line = text_cell{i_line};
+         
+         % remove initial white space
+         while ~isempty(this_line) && (this_line(1) == ' ' || double(this_line(1)) == 9)
+             this_line(1) = [];
+         end
+         outlier_removal_cell{i_line} = this_line; %#ok<AGROW>
+         
+         % remove comments
+         if length(this_line) > 1 && any(ismember(this_line, '#'))
+             lines_to_prune(i_line) = true;
+         end
+         
+         % remove lines that do not match this processed folder
+         this_line_pieces = strsplit(this_line, ',');
+         if ~strcmp(directory_pieces{end},this_line_pieces{1})
+              lines_to_prune(i_line) = true;
+         end
+         
+         % flag lines consisting only of white space
+         if all(ismember(this_line, ' ') | double(this_line) == 9)
+             lines_to_prune(i_line) = true;
+         end
+         
+     end
+     outlier_removal_cell(lines_to_prune) = [];
+     
+     
+    
     
     if size(condition_onset_files,1) ~= size(files_to_test,1)
         error('Need the same # of Onset Files as Functional Runs')
@@ -41,7 +76,31 @@ function level_one_stats(TR_from_json)
         this_file_with_volumes = spm_select('expand', files_to_test(i_run,:));
         this_art_regression_file = art_regression_outlier_files(i_run,:);
         
-        matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).scans = cellstr(this_file_with_volumes);
+        % check to see if there were any outlier removals for this run
+        % remove lines that do not match this processed folder
+        for this_line = 1:size(outlier_removal_cell,2)
+             line_pieces_per_run = strsplit(outlier_removal_cell{this_line}, ',');
+             this_line_run_matches(this_line) =  str2num(line_pieces_per_run{2}) ==  i_run;
+        end
+
+        if sum(this_line_run_matches > 1)
+            error 'Should not have more than one line for each run in same processed folder in otlier_removal_settings.txt'
+        end
+        
+        this_line_match_index = find(this_line_run_matches);
+       
+        this_line = outlier_removal_cell{this_line_match_index};
+        
+        this_line_pieces = strsplit(this_line, ',');
+       
+        
+        if (i_run == str2num(this_line_pieces{2}))
+            this_run_start_volume = this_line_pieces{3};
+            this_run_time_correction = TR_from_json * str2double(this_run_start_volume);
+        else
+            this_run_start_volume = 0;
+            this_run_time_correction = 0;
+        end
         
         this_condition_onset_file = condition_onset_files(i_run,:);
         this_onset_data = readtable(this_condition_onset_file);
@@ -54,20 +113,44 @@ function level_one_stats(TR_from_json)
             
             this_condition_name_onset_indices = find(contains(this_run_condition_names,unique_condition_names(i_cond)));
             this_cond_onset_times =  this_onset_data(this_condition_name_onset_indices,:).condition_onset;
-            
             this_cond_rest_times = this_onset_data(this_condition_name_onset_indices,:).rest_onset;
             
-            this_cond_durations = this_cond_rest_times - this_cond_onset_times;
-            
-            if any(mod(this_cond_durations,1000) )
-                error('check the condition onset values')
+            if (numel(num2str(this_cond_onset_times)) < 4 )
+                error('check the condition onset values (expecting ms)')
             else
                 this_cond_onset_times = this_cond_onset_times / 1000;
-                this_cond_durations = this_cond_durations / 1000;
+                this_cond_rest_times = this_cond_rest_times / 1000;
+            end
+            
+            % correct for the start volume if necessary
+            this_cond_onset_times_corrected = this_cond_onset_times - this_run_time_correction;
+            this_cond_rest_times_corrected = this_cond_rest_times - this_run_time_correction;
+            
+            if any(this_cond_onset_times_corrected < 0)
+                negative_onset_indices = find(this_cond_onset_times_corrected < 0)
+                negative_rest_indices = find(this_cond_rest_times_corrected < 0)
+                
+                %correct negative onset times
+                this_cond_onset_times_corrected(negative_onset_indices) = 0;
+                this_cond_rest_times_corrected(negative_rest_indices) = 0;
+            end
+            
+            % check to see if the onset and duration are divisible by TR..
+            % if not, increase onset time and decrease duration
+            while (mod(this_cond_onset_times_corrected, 1.5) ~= 0)
+                this_cond_onset_times_corrected = round(this_cond_onset_times_corrected, 1) + 0.1;
+            end
+            
+            this_cond_durations = this_cond_rest_times_corrected - this_cond_onset_times_corrected;
+            
+            while (mod(this_cond_durations, 1.5)  ~= 0)
+                this_cond_durations = round(this_cond_durations, 1) - 0.1;
             end
      
+            
+            matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).scans = cellstr(this_file_with_volumes);
             matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).cond(i_cond).name = char(unique_condition_names(i_cond));
-            matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).cond(i_cond).onset = this_cond_onset_times;
+            matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).cond(i_cond).onset = this_cond_onset_times_corrected;
             matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).cond(i_cond).duration = this_cond_durations;
             matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).cond(i_cond).tmod = 0;
             matlabbatch{1}.spm.stats.fmri_spec.sess(i_run).cond(i_cond).pmod = struct('name', {}, 'param', {}, 'poly', {});
@@ -155,7 +238,7 @@ this_contrast_array = {};
                           contrast_zero_padded(this_contrast_cond_left_SPMindex_this_run * 2 - 1) = 1;
                           contrast_zero_padded(this_contrast_cond_right_Run1_SPMindex_this_run * 2 - 1) = -1;
                           
-                          
+               
                           % matlab does not like this ...
                           if i_run < number_of_runs
                               if ~isempty(this_contrast_array)
