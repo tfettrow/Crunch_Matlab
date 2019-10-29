9559c40abf1c82eefabf997dcb099e3073ce9e47
tfettrow <tfettrow@gmail.com>
2019-10-29 10:19:20 -0400

    changes voxel write size of T1 norm

--- scripts/spm_norm_t1.m
+++ scripts/spm_norm_t1.m
@@ -10,7 +10,7 @@ files_to_normalise = spm_select('ExtFPList', data_path, '^mT1.*\.nii$');
 matlabbatch{1}.spm.spatial.normalise.write.subj.def  = cellstr('y_T1.nii');
 matlabbatch{1}.spm.spatial.normalise.write.subj.resample = cellstr(files_to_normalise);
 
-matlabbatch{1}.spm.spatial.normalise.write.woptions.vox  = [2 2 2];
+matlabbatch{1}.spm.spatial.normalise.write.woptions.vox  = [1 1 1];
 matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'normalized_';
 
 spm_jobman('run',matlabbatch);
