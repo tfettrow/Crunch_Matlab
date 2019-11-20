4c93d846ee1f29f5525b15151f9b45737c06c129
tfettrow <tfettrow@gmail.com>
2019-11-20 13:18:48 -0500

    figs for each roi looping through subject..

--- scripts/create_roi_results_figure.m
+++ scripts/create_roi_results_figure.m
@@ -1,9 +1,11 @@
 % roi_results figure generation
-
+clear,clc
+close all
 data_path = pwd;
 
+handles.handle_plot = [];
 directory_pieces = regexp(data_path,filesep,'split');
-levels_back = 2; % assuming only working in ANTS folder atm
+levels_back = 1; % assuming only working in ANTS folder atm
 subject_level_directory = getfield( directory_pieces, {1:length(directory_pieces)-levels_back} );
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -25,7 +27,17 @@ subject_level_directory = getfield( directory_pieces, {1:length(directory_pieces
 %     end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-data = readtable('1002_roi_results.txt');
+% find each subject roi_results file
+roi_results_file_list = dir('*roi_results.txt');
+
+for this_subject_results_file = 1 : length(roi_results_file_list)
+    this_subject_color = rand(1,3);
+    
+%     this_subject_results_file
+   data = readtable(roi_results_file_list(this_subject_results_file).name);
+%      data = readtable('1002_roi_results.txt');
+
+
 
 roi_condition_name = data.Var1;
 roi_beta_value = data.Var2;
@@ -60,55 +72,37 @@ for this_unique_roi_index = 1 : length(unique_rois)
         [tf, idx] =ismember(condition_order, roi_condition_name_matrix(indices_this_roi, 4));
         this_cond_ordered_roi_beta_value = roi_beta_value(indices_this_roi(1) - 1 + [idx idx+length(idx)]); %doubling up the idx to average between long and short unless there is a reason to look at differences between long and short
         ordered_roi_beta_value = [ordered_roi_beta_value this_cond_ordered_roi_beta_value];
-        ordered_roi_condition_name_matrix = [ordered_roi_condition_name_matrix; roi_condition_name_matrix(indices_this_roi(1) - 1 + [idx idx+length(idx)], : )];
+        ordered_roi_condition_name_matrix{this_unique_roi_index} = (roi_condition_name_matrix(indices_this_roi(1) - 1 + [idx idx+length(idx)], : ));
     end
 end
 
-
-
-% save condition_order and ordered_roi_beta_value to results.mat
-
-%LDLPFC
-figure; plot(roi_beta_value(1:8))
-title('LDLPFC')
-ylabel('Beta Weight')
-xlabel('Nback')
-xticklabels({'Long One','Long Three','Long Two','Long Zero','Short One','Short Three','Short two','Short Zero'})
-xtickangle(45)
-
-%LIPS
-figure; plot(roi_beta_value(9:16))
-title('LIPS')
-ylabel('Beta Weight')
-xlabel('Nback')
-xticklabels({'Long One','Long Three','Long Two','Long Zero','Short One','Short Three','Short two','Short Zero'})
-xtickangle(45)
-%LM1
-figure; plot(roi_beta_value(17:24))
-title('LM1')
-ylabel('Beta Weight')
-xlabel('Nback')
-xticklabels({'Long One','Long Three','Long Two','Long Zero','Short One','Short Three','Short two','Short Zero'})
-xtickangle(45)
-%RDLPFC
-figure; plot(roi_beta_value(25:32))
-title('RDLPFC')
-ylabel('Beta Weight')
-xlabel('Nback')
-xticklabels({'Long One','Long Three','Long Two','Long Zero','Short One','Short Three','Short two','Short Zero'})
-xtickangle(45)
-%RIPS
-figure; plot(roi_beta_value(33:40))
-title('RIPS')
-ylabel('Beta Weight')
-xlabel('Nback')
-xticklabels({'Long One','Long Three','Long Two','Long Zero','Short One','Short Three','Short two','Short Zero'})
-xtickangle(45)
-%RM1
-figure; plot(roi_beta_value(41:48))
-title('RM1')
-ylabel('Beta Weight')
-xlabel('Nback')
-xticklabels({'Long One','Long Three','Long Two','Long Zero','Short One','Short Three','Short two','Short Zero'})
-xtickangle(45)
+this_figure_number = 1;
+for this_roi = 1 : length(unique_rois)
+    if strcmp(subject_level_directory{end}, '06_Nback')
+        for this_isi = {'short', 'long'}
+            this_isi_indices = contains(ordered_roi_condition_name_matrix{this_roi}(:,3), this_isi)
+            
+            x_num = [1 : sum(this_isi_indices)];
+            x_label = ordered_roi_condition_name_matrix{this_roi}(this_isi_indices,4);
+            y = ordered_roi_beta_value(this_isi_indices,this_roi)';
+            
+            this_figure_handle = fprintf('%s',unique_rois{this_roi})
+            
+           if (length(handles.handle_plot) == length(unique_rois) * 2)
+               figure(handles.handle_plot(this_figure_number))
+           else
+               handles.handle_plot(this_figure_number) = figure;
+           end
+           hold on;
+           plot(x_num, y, 'o', 'MarkerFaceColor', this_subject_color, 'MarkerSize',10)
+           xticks([x_num])
+           xlim([0 5])
+           title([unique_rois(this_roi) this_isi])
+           xtickangle(45)
+           set(gca,'xticklabel',x_label.')
+           this_figure_number = this_figure_number + 1;
+        end
+    end
+end
     
+end
