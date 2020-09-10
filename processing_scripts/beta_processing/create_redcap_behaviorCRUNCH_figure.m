8253eb69faa58bdb0dbfa55f7d807246414dd335
tfettrow <tfettrow@gmail.com>
2020-09-10 13:46:24 -0400

    updated axes

--- processing_scripts/beta_processing/create_redcap_behaviorCRUNCH_figure.m
+++ processing_scripts/beta_processing/create_redcap_behaviorCRUNCH_figure.m
@@ -59,9 +59,12 @@ for this_group_index = 1 : length(group_names)
             [r , p] = corr(results(this_group_subjectindices,this_roi_index), this_subject_400m_data(this_group_subjectindices)');
             r2 = r^2;
             coefs = polyfit(results(this_group_subjectindices,this_roi_index)', this_subject_400m_data(this_group_subjectindices), 1);
-            fittedX=linspace(1, 4, 100);
+            fittedX=linspace(0, 5, 100);
             fittedY=polyval(coefs, fittedX);
             plot(fittedX, fittedY, '-', 'Color',group_color_matrix(this_group_index, :),'LineWidth',1);
+            if this_figure_number == 1
+                ylabel('Walking Time (seconds)')
+            end
             this_figure_number = this_figure_number+1;
         elseif any(strcmp(task_folder, '06_Nback'))
             subplot(1, 4, this_figure_number); hold on;
@@ -78,29 +81,14 @@ for this_group_index = 1 : length(group_names)
             fittedY2=polyval(coefs2, fittedX);
             plot(fittedX, fittedY1, '-', 'Color',group_color_matrix(this_group_index, :),'LineWidth',1);
             plot(fittedX, fittedY2, '-', 'Color',(group_color_matrix(this_group_index, :)+.2)*.5,'LineWidth',1);
+            if this_figure_number == 1
+                ylabel('Walking Time (seconds)')
+            end
             this_figure_number = this_figure_number+1;
         end
         title([unique_rois(this_roi_index)],'interpreter','latex')
-        
-        
-%         x1 = 0.1;
-%         y1 = min(allYLim) + min(allYLim) * .25;
-%         y2 = min(allYLim) + min(allYLim) * .2;
-%         y3 = min(allYLim) + min(allYLim) * .1;
-%         y4 = min(allYLim) + min(allYLim) * .05;
-%         text1 = ['OA r^2 = ' num2str(r2)];
-%         text2 = ['OA m = ' num2str(coefs(1))];
-%         text3 = ['YA r^2 = ' num2str(r2_ya)];
-%         text4 = ['YA m = ' num2str(coefs_ya(1))];
-        
-% %         text(x1,y1,text1)
-% set(gca, 'XLim', [0 5]
-xticks([1:4])
+        xticks([1:4])
         xlim([0 5])
-%      
-%         thisYLim = get(gca, 'YLim');
-%         allYLim = [allYLim thisYLim];
-            ylim([200 550])
-%         set(gca, 'YLim', [min(allYLim), max(allYLim)]);
+        ylim([200 550])
     end
 end
