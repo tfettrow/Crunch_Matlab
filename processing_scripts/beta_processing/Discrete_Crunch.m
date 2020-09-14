24a643dd480f32237bd01c0af1bc1a15eaa12bbb
valay93 <valayshahlab@gmail.com>
2020-09-14 18:24:56 -0400

    Discrete crunch analysis file created. the variable "cr" in the .mat file contains the discrete difficulty level at which the participant CRUNCHed 0 = never CRUNCHed (diff level 4 had the highest beta), 1 = they continuously decreased in beta from the start (CRUNCHed from start), 2-3 = CRUNCH occured the order of the CRUNCH point in "cr" is same as yours e.g. [1,2,3,3] = [L-acc,L-dlpfc,R-acc,R-dlpfc]

--- processing_scripts/beta_processing/Discrete_Crunch.m
+++ processing_scripts/beta_processing/Discrete_Crunch.m
@@ -5,17 +5,26 @@ clear,clc
 
 %% settings
 %select the task 1 = MOTO, 2 = nback
-task = 1;
+task = 2;
+subj = 2;
 
 if task == 1
     task_folder='05_MotorImagery';
-    subjects = [1002,1004,1010,1011,1013,1009];
+    if subj == 1
+        subjects = [1002,1004,1010,1011,1013,1009];
+    elseif subj == 2
+        subjects =  [2002,2007,2008,2012,2013,2015,2018,2020,2021,2022,2023,2025,2026,2033,2034];
+    end
 elseif task == 2
     task_folder='06_Nback';
-    subjects =  [2002,2007,2008,2012,2013,2015,2018,2020,2021,2022,2023,2025,2026,2033,2034];
+    if subj == 1
+        subjects = [1002,1004,1010,1011,1013,1009];
+    elseif subj == 2
+        subjects =  [2002,2007,2008,2012,2013,2015,2018,2020,2021,2022,2023,2025,2026,2033,2034];
+    end
 end
 
-Results_filename='CRUNCH_discrete_vas.mat';
+Results_filename='CRUNCH_discrete.mat';
 
 save_variables = 1;
 no_labels = 0;
@@ -25,14 +34,14 @@ data_path = 'Z:\share\FromExternal\Research_Projects_UF\CRUNCH\MiM_Data'; % chan
 %subject_color_matrix = distinguishable_colors(length(subjects));
 
 
-for sub = 1
-    %%create file path for beta values
+for sub = 1:length(subjects)
+    %create file path for beta values
     subj_results_dir = fullfile(data_path, num2str(subjects(sub)), 'Processed', 'MRI_files', task_folder, 'ANTS_Normalization', 'Level1_WholeBrain');
     this_subject_roiResults_path = fullfile(subj_results_dir, strcat(num2str(subjects(sub)),'_fmri_redcap.csv'));
     
     fileID = fopen(this_subject_roiResults_path);
     
-    %%read the csv file and reshape to have separate headers and values
+    %read the csv file and reshape to have separate headers and values
     data = textscan(fileID,'%s','delimiter',',');
     data = reshape(data{:},length(data{1})/2,2);
     
@@ -41,171 +50,78 @@ for sub = 1
         if task == 1
             ordered_conditions{this_beta-2} = split_difficulty{1}; %difficulty level = flat to high
             roi_names{this_beta-2} = strcat(split_difficulty{2},'_',split_difficulty{3}); %brain region name, l-pfc, r-pfc, l-acc, r-acc
-            ordered_beta{this_beta-2} = data{this_beta,2}; %beta value arranged here
         elseif task == 2
             ordered_conditions{this_beta-2} = strcat(split_difficulty{1},'_',split_difficulty{2}); % difficulty = 0 to 3 with long or short ISI
             roi_names{this_beta-2} = strcat(split_difficulty{3},'_',split_difficulty{4}); %brain region name
-            ordered_beta{this_beta-2} = data{this_beta,2}; %beta value
         end
     end
+    
     unique_rois = unique(roi_names); %delete the repeats of the brain region name
     
     this_figure_number = 1;
+    figure;
+    
     for this_roi_index = 1 : length(unique_rois) %1:4, l-pfc, r-pfc, l-acc, r-acc
         this_roi_indices = find(strcmp(roi_names, unique_rois{this_roi_index})); %find the index of when the current ROI occurs
         
-        temp = ordered_beta(:,this_roi_indices)'; %temporary hold the beta values
-        for i_beta= 1:length(temp)
-            beta_values(:,i_beta) = textscan(temp{i_beta},'%f'); %beta values for current ROI
+        for k = 1:length(this_roi_indices)
+            temp(1,k) = textscan(data{this_roi_indices(k)+2,2},'%f'); %temporary hold the beta value
         end
-        beta_values = cell2mat(beta_values);
+        beta_values = cell2mat(temp)'; %covert from cell to matrix
         subplot(1, 4, this_figure_number);
         hold on;
         
         if any(strcmp(task_folder, '05_MotorImagery'))
-            number_of_levels = 1:4;
+            number_of_levels = 1:4; %difficulty levels (x axis)
             plot(number_of_levels,beta_values,'-or');
-            mm = find(max(beta_values)==beta_values);
-            if (mm < 2 || mm > 3)
-                cr(this_roi_index) = 0;
+            mm = find(max(beta_values)==beta_values); %find the index of max beta value
+            if (mm > 3) %ifthe subject has a CRUNCH point
+                cr(this_roi_index) = 0; %no CRUNCH point
             else
-                cr(this_roi_index) = mm;
+                cr(this_roi_index) = mm; %CRUNCH point
             end
-
-%             coeffs=polyfit(number_of_levels, beta_values, 2);
-%             fittedX=linspace(min(number_of_levels), max(number_of_levels), 100);
-%             fittedY=polyval(coeffs, fittedX);
-%             
-%             plot(number_of_levels, beta_values,'o', 'MarkerFaceColor', subject_color_matrix(sub, :), 'MarkerEdgeColor', subject_color_matrix(sub, :), 'MarkerSize', 3)
-%             plot(fittedX, fittedY, '--', 'Color', subject_color_matrix(sub, :),'LineWidth',1);
-%             
-%             % identify crunch point in terms of difficulty level
-%             [crunchpoint_y, crunchpoint_percent_of_fit(sub,this_roi_index)] = max(fittedY);
-%             crunchpoint_x(this_roi_index) = fittedX(crunchpoint_percent_of_fit(sub,this_roi_index));
-%             
-%             scatter(fittedX(crunchpoint_percent_of_fit(sub,this_roi_index)), fittedY(crunchpoint_percent_of_fit(sub,this_roi_index)), 100,  'o', 'MarkerFaceColor',  subject_color_matrix(sub, :), 'MarkerEdgeColor', subject_color_matrix(sub, :), 'MarkerFaceAlpha',3/8); % 'MarkerSize', 12)
-       
         elseif any(strcmp(task_folder, '06_Nback'))
             number_of_levels = 1:4;
-            slope_cr = diff(beta_values)./diff(number_of_levels); %find slope
+            number_of_levels2 = 5:8;
+            
             plot(number_of_levels,beta_values(1:4),'-or');
             plot(number_of_levels,beta_values(5:8),'-.or');
-            mm = find(max(beta_values)==beta_values);
-            if (mm < 2 || mm > 3)
-                cr(this_roi_index) = 0;
+            mm1 = find(max(beta_values(1:4))==beta_values(1:4)); %CRUNCH for 500 ISI
+            if (mm1 > 3)
+                cr_500(this_roi_index) = 0;
+                
             else
-                cr(this_roi_index) = mm;
+                cr_500(this_roi_index) = mm1;
+                
             end
-            
-%             coeffs1=polyfit(number_of_levels, beta_values(1:4), 2);
-%             coeffs2=polyfit(number_of_levels, beta_values(5:8), 2);
-%             fittedX=linspace(min(number_of_levels), max(number_of_levels), 100);
-%             fittedY1=polyval(coeffs1, fittedX);
-%             fittedY2=polyval(coeffs2, fittedX);
-%             
-%             
-%             plot(fittedX, fittedY1, '--', 'Color', subject_color_matrix(sub, :),'LineWidth',1);
-%             plot(fittedX, fittedY2, '-', 'Color', subject_color_matrix(sub, :),'LineWidth',1);
-%             plot(number_of_levels, beta_values(1:4),'o', 'MarkerFaceColor', subject_color_matrix(sub, :), 'MarkerEdgeColor', subject_color_matrix(sub, :), 'MarkerSize', 3)
-%             plot(number_of_levels, beta_values(5:8),'o', 'MarkerFaceColor', subject_color_matrix(sub, :), 'MarkerEdgeColor', subject_color_matrix(sub, :), 'MarkerSize', 3)
-%             
-%             [crunchpoint_y1, crunchpoint_percent_of_fit_1(sub,this_roi_index)] = max(fittedY1);
-%             [crunchpoint_y2, crunchpoint_percent_of_fit_2(sub,this_roi_index)] = max(fittedY2);
-%             crunchpoint_x1(this_roi_index) = fittedX(crunchpoint_percent_of_fit_1(sub,this_roi_index));
-%             crunchpoint_x2(this_roi_index) = fittedX(crunchpoint_percent_of_fit_2(sub,this_roi_index));
-%             
-%             scatter(fittedX(crunchpoint_percent_of_fit_1(sub,this_roi_index)), fittedY1(crunchpoint_percent_of_fit_1(sub,this_roi_index)), 100,  'o', 'MarkerFaceColor',  subject_color_matrix(sub, :), 'MarkerEdgeColor', subject_color_matrix(sub, :), 'MarkerFaceAlpha',3/8); % 'MarkerSize', 12,
-%             scatter(fittedX(crunchpoint_percent_of_fit_2(sub,this_roi_index)), fittedY2(crunchpoint_percent_of_fit_2(sub,this_roi_index)), 100,  'o', 'MarkerFaceColor',  subject_color_matrix(sub, :), 'MarkerEdgeColor', subject_color_matrix(sub, :), 'MarkerFaceAlpha',3/8); % 'MarkerSize', 12,
+            mm2 = find(max(beta_values(5:8))==beta_values(5:8)); %CRUNCH for 1500 ISI
+            if (mm2 > 3)
+                cr_1500(this_roi_index) = 0;
+            else
+                cr_1500(this_roi_index) = mm2;
+            end
         end
+        hold off;
         xticks(number_of_levels)
         xlim([0 5])
-        title(unique_rois(this_roi_index),'interpreter','latex')
+        title(char(unique_rois(this_roi_index)))
         this_figure_number = this_figure_number + 1;
         ylabel('beta value')
-        clearvars beta_values;
+        %         clearvars beta_values;
     end
+    hold off;
+    
     if save_variables
         if any(strcmp(task_folder, '05_MotorImagery'))
             task='MotorImagery';
-            save(char(strcat(subj_results_dir,filesep,strcat(num2str(subjects(sub)),'_',task,'_',Results_filename))));
+            save(char(strcat(subj_results_dir,filesep,strcat(num2str(subjects(sub)),'_',task,'_',Results_filename))),'cr*','data','unique_rois');
         elseif any(strcmp(task_folder, '06_Nback'))
             task='Nback';
-            save(char(strcat(subj_results_dir,filesep,strcat(num2str(subjects(sub)),'_',task,'_',Results_filename))));
+            save(char(strcat(subj_results_dir,filesep,strcat(num2str(subjects(sub)),'_',task,'_',Results_filename))),'cr*','data','unique_rois');
         end
     end
+    fclose(fileID);
+    clearvars beta_values cr* data temp ordered_conditions this_beta this_roi_index;
 end
 
-allYLim = [];
-for this_subplot = 1 : this_figure_number  - 1
-    subplot(1, 4, this_subplot);
-    if this_subplot > 1
-        ylabel([])
-    end
-    
-    h = findobj(gca,'Type','line');
-    thisYLim = get(gca, 'YLim');
-    allYLim = [allYLim thisYLim];
-    
-    if no_labels
-        set(get(gca, 'xlabel'), 'visible', 'off');
-        set(get(gca, 'ylabel'), 'visible', 'off');
-        set(get(gca, 'title'), 'visible', 'off');
-        set(gca, 'xticklabel', '');
-        set(gca, 'yticklabel', '');
-        legend(gca, 'hide');
-    end
-end
-% MaximizeFigureWindow;
 
-figure;hold on;
-title('Correlations between left_acc and right_acc', 'interpreter','latex')
-% ylabel('right_acc', 'interpreter','latex')
-% xlabel('left_acc', 'interpreter','latex')
-
-xlim([0 100])
-ylim([0 100])
-
-if any(strcmp(task_folder, '05_MotorImagery'))
-    coeffs1=polyfit(crunchpoint_percent_of_fit(:,1), crunchpoint_percent_of_fit(:,3), 1);
-    fittedX=linspace(0, 100, 100);
-    fittedY1=polyval(coeffs1, fittedX);
-    plot(crunchpoint_percent_of_fit(:,1), crunchpoint_percent_of_fit(:,3), 'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b', 'MarkerSize', 12)
-    plot(fittedX, fittedY1, '--', 'Color', 'k','LineWidth',1);
-    [r , p] = corr(crunchpoint_percent_of_fit(:,1), crunchpoint_percent_of_fit(:,3));
-    r2 = r^2;
-    thisXLim = get(gca, 'XLim');
-    thisYLim = get(gca, 'YLim');
-    x1 = 45;
-    y1 = 95;
-    y2 = 90;
-    y3 = min(allYLim) + min(allYLim) * .1;
-    y4 = min(allYLim) + min(allYLim) * .05;
-    text1 = ['r^2 = ' num2str(r2)];
-    text2 = ['m = ' num2str(coeffs1(1))];
-    text(x1,y1,text1)
-    text(x1,y2,text2)
-elseif any(strcmp(task_folder, '06_Nback'))
-    coeffs1=polyfit(crunchpoint_percent_of_fit_1(:,1), crunchpoint_percent_of_fit_1(:,1), 1);
-    coeffs2=polyfit(crunchpoint_percent_of_fit_2(:,1), crunchpoint_percent_of_fit_2(:,1), 1);
-    fittedX=linspace(0, 100, 100);
-    fittedY1=polyval(coeffs1, fittedX);
-    fittedY2=polyval(coeffs2, fittedX);
-    plot(crunchpoint_percent_of_fit_1(:,1), crunchpoint_percent_of_fit_1(:,3), 'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b', 'MarkerSize', 12)
-    plot(fittedX, fittedY1, '--', 'Color', 'k','LineWidth',1);
-    plot(crunchpoint_percent_of_fit_2(:,1), crunchpoint_percent_of_fit_2(:,3), 'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b', 'MarkerSize', 12)
-    plot(fittedX, fittedY1, '--', 'Color', 'k','LineWidth',1);
-    plot(fittedX, fittedY2, '--', 'Color', 'k','LineWidth',1);
-    [r , p] = corr([crunchpoint_percent_of_fit_1(:,1); crunchpoint_percent_of_fit_2(:,1)], [crunchpoint_percent_of_fit_1(:,3); crunchpoint_percent_of_fit_2(:,3)]);
-    r2 = r^2;
-    thisXLim = get(gca, 'XLim');
-    thisYLim = get(gca, 'YLim');
-    x1 = 45;
-    y1 = 95;
-    y2 = 90;
-    y3 = min(allYLim) + min(allYLim) * .1;
-    y4 = min(allYLim) + min(allYLim) * .05;
-    text1 = ['r^2 = ' num2str(r2)];
-    text2 = ['m = ' mean([num2str(coeffs1(1)) num2str(coeffs2(1))])];
-    text(x1,y1,text1)
-    text(x1,y2,text2)
-end
