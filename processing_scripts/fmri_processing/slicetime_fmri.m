f12fce1313a7aa45c26b4031200fb683cc243485
tfettrow <tfettrow@gmail.com>
2020-12-08 15:24:37 -0500

    Update slicetime_fmri.m

--- processing_scripts/fmri_processing/slicetime_fmri.m
+++ processing_scripts/fmri_processing/slicetime_fmri.m
@@ -26,22 +26,26 @@ all_files_to_slicetime = spm_select('FPList', data_path, '^fMRI.*\.nii$');
 
 for i_file = 1 : size(all_files_to_slicetime,1)
     this_file_with_volumes = spm_select('expand', all_files_to_slicetime(i_file,:));
-    all_scans_fullpath_scan_cell{1, i_file} = cellstr(this_file_with_volumes);
+    all_scans_fullpath_scan_cell = cellstr(this_file_with_volumes);
+    
+    matlabbatch{1}.spm.temporal.st.scans = {(all_scans_fullpath_scan_cell)};
+
+    [stMsec, TRsec] = bidsSliceTiming(json_path_names(i_file,:));
+    if isempty(stMsec) || isempty(TRsec), error('Update dcm2niix? Unable determine slice timeing or TR from BIDS file %s', BIDSname); end;
+    
+    reftime = max(stMsec)/2;
+    
+    timing = [0 TRsec];
+    matlabbatch{1}.spm.temporal.st.nslices = length(stMsec);
+    matlabbatch{1}.spm.temporal.st.tr = TRsec;
+    matlabbatch{1}.spm.temporal.st.ta = 0;
+    matlabbatch{1}.spm.temporal.st.so = stMsec;
+    matlabbatch{1}.spm.temporal.st.refslice = reftime;
+    matlabbatch{1}.spm.temporal.st.prefix = 'slicetimed_';
+    
+    spm_jobman('run',matlabbatch);
+    
+    clear matlabbatch
 end    
-matlabbatch{1}.spm.temporal.st.scans = all_scans_fullpath_scan_cell;
 
-[stMsec, TRsec] = bidsSliceTiming(json_path_names(i_file,:));
-if isempty(stMsec) || isempty(TRsec), error('Update dcm2niix? Unable determine slice timeing or TR from BIDS file %s', BIDSname); end;
-
-reftime = max(stMsec)/2;
-
-timing = [0 TRsec];
-matlabbatch{1}.spm.temporal.st.nslices = length(stMsec);
-matlabbatch{1}.spm.temporal.st.tr = TRsec;
-matlabbatch{1}.spm.temporal.st.ta = 0;
-matlabbatch{1}.spm.temporal.st.so = stMsec;
-matlabbatch{1}.spm.temporal.st.refslice = reftime;
-matlabbatch{1}.spm.temporal.st.prefix = 'slicetimed_';
-
-spm_jobman('run',matlabbatch);
 
