b8c7b8e2d44f4c9737858dde6710f3e055ea0a0f
valay93 <69319816+valay93@users.noreply.github.com>
2021-02-17 16:33:29 -0500

    updated age_get to output age range
    add a 4th output argument to get the age range in an array

--- processing_scripts/age_get.m
+++ processing_scripts/age_get.m
@@ -1,4 +1,4 @@
-function [meanage,stdage,gend]=age_get(subjects)
+function [meanage,stdage,gend,agerange]=age_get(subjects)
 %subjects = {'1002','1004','1007','1009','1010','1011','1013','1020','1022','1024','1027','2002','2007','2008','2012','2013','2015','2017','2018','2020','2021','2022','2023','2025','2026','2033','2034','2037','2042','2052'};
 for i = 1:length(subjects)
     subj_results_dir = fullfile(pwd, subjects{i});
@@ -7,6 +7,7 @@ for i = 1:length(subjects)
 end
 meanage = mean(age);
 stdage = std(age);
+[agerange(1),agerange(2)] = bounds(age);
 g = cell2mat(gender);
 gend = length(find(g == 'F'));
 end
