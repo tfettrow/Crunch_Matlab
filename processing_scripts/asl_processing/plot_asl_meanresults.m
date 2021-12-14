f5b4dc525631b5541066d07d70f005aef6410f83
tfettrow <tfettrow@gmail.com>
2021-12-14 15:36:28 -0500

    plot asl stuff

--- processing_scripts/asl_processing/plot_asl_meanresults.m
+++ processing_scripts/asl_processing/plot_asl_meanresults.m
@@ -14,6 +14,7 @@
 function plot_asl_meanresults(varargin)
 % plot_fsl_histogram('subject','1002','roi_settings_filename','ROI_settings_MiMRedcap_CAT12_NewAcc.txt'
 % WARNING: currently setup to require each group run seperately
+%  plot_asl_meanresults('subjects', {'1002' '1004'  '1009' '1010'  '1013' '1018' '1019' '1020' '1022'  '1026' '1027' '2002' '2007' '2008' '2012' '2013' '2015' '2018' '2020' '2021' '2022' '2023' '2025' '2026' '2033' '2034' '2037' '2038' '2039' '2042' '2052' '2059' '2027' '3006' '3007' '3008' '3010' '3021' '3023' '3024' '3025' '3026' '3028' '3029' '3036' '3039' '3040' '3042' '3043' '3046' '3047' '3051' '3053' '3058' '3059' '3063' '3066' '3068'},'group_names',{'ya','hoa','loa'},'group_ids',[1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3],'save_figures',1, 'no_labels', 0, 'save_scores',0, 'results_foldername', 'BasilCMD_calib_anat_scalib_pvcorr', 'pvcorr', 0)
 
 parser = inputParser;
 parser.KeepUnmatched = true;
@@ -55,6 +56,39 @@ for this_subject_index = 1:length(subjects)
     end
 end
 
+
+figure; hold on;
+for this_group_index = 1 : length(group_names)
+    this_group_subjectindices = find(group_ids==this_group_index);
+    this_group_and_roi_vol_results = gm_perf_data(end,this_group_subjectindices);
+    
+    singleBoxPlot(this_group_and_roi_vol_results,'abscissa', this_group_index, 'EdgeColor',color_groups(this_group_index,:), 'MarkerColor',color_groups(this_group_index,:),'WiskColor',color_groups(this_group_index,:), 'MeanColor',color_groups(this_group_index,:))
+    
+end
+xlim([ 0 length(this_group_index+1)])
+xlim([0 length(group_names)+1])
+
+ylabel('Perfusion (ml/100g/min)')
+set(gca, 'XTick',1:length(group_names),'xticklabel', group_names,'TickLabelInterpreter','none','FontSize',8)
+%     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+title('AVG GM CBF','interpreter','latex','FontSize',16)
+xtickangle(45)
+% if no_labels
+%     set(gcf, 'ToolBar', 'none');
+%     set(gcf, 'MenuBar', 'none');
+%     set(get(gca, 'xlabel'), 'visible', 'off');
+%     set(get(gca, 'ylabel'), 'visible', 'off');
+%     set(get(gca, 'title'), 'visible', 'off');
+%     legend(gca, 'hide');
+% end
+if save_figures
+    fig_title = strcat('group_GM_CBF');
+    
+    filename =  fullfile(project_path, 'figures', fig_title);
+    saveas(gca, filename, 'tiff')
+end
+
+
 figure; hold on;
 for this_group_index = 1 : length(group_names)
     this_group_subjectindices = find(group_ids==this_group_index);
@@ -62,15 +96,50 @@ for this_group_index = 1 : length(group_names)
     bar(this_group_subjectindices, gm_perf_data(end,this_group_subjectindices), 'FaceColor', color_groups(this_group_index,:)); hold on;
 end
 
-title('AVG GM CBF','interpreter','none', 'FontSize',18)
-ylabel('AVG GM CBF (?)','interpreter','none')
-set(gca,'XTick',1:length(subjects),'xticklabel',subjects,'TickLabelInterpreter','none')
+set(gca,'XTick',1:length(subjects),'xticklabel',subjects, 'fontsize',8,'TickLabelInterpreter','none')
 xtickangle(45)
 set(gcf, 'ToolBar', 'none');
 set(gcf, 'MenuBar', 'none');
+title('AVG GM CBF','interpreter','none', 'FontSize',16)
+ylabel('AVG GM CBF (ml/100g/min)','interpreter','none')
 
 if save_figures
+    MaximizeFigureWindow
     fig_title = 'subjects_GM_CBF';
+    filename =  fullfile(project_path, 'figures', fig_title);
+    saveas(gca, filename, 'tiff')
+    close 
+end
+
+
+
+figure; hold on;
+for this_group_index = 1 : length(group_names)
+    this_group_subjectindices = find(group_ids==this_group_index);
+    this_group_and_roi_vol_results = wm_perf_data(end,this_group_subjectindices);
+    
+    singleBoxPlot(this_group_and_roi_vol_results,'abscissa', this_group_index, 'EdgeColor',color_groups(this_group_index,:), 'MarkerColor',color_groups(this_group_index,:),'WiskColor',color_groups(this_group_index,:), 'MeanColor',color_groups(this_group_index,:))
+    
+end
+xlim([ 0 length(this_group_index+1)])
+xlim([0 length(group_names)+1])
+
+ylabel('Perfusion (ml/100g/min)')
+set(gca, 'XTick',1:length(group_names),'xticklabel', group_names,'TickLabelInterpreter','none','FontSize',8)
+%     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+title('AVG WM CBF','interpreter','latex','FontSize',16)
+xtickangle(45)
+% if no_labels
+%     set(gcf, 'ToolBar', 'none');
+%     set(gcf, 'MenuBar', 'none');
+%     set(get(gca, 'xlabel'), 'visible', 'off');
+%     set(get(gca, 'ylabel'), 'visible', 'off');
+%     set(get(gca, 'title'), 'visible', 'off');
+%     legend(gca, 'hide');
+% end
+if save_figures
+    fig_title = strcat('group_WM_CBF');
+    
     filename =  fullfile(project_path, 'figures', fig_title);
     saveas(gca, filename, 'tiff')
 end
@@ -83,19 +152,52 @@ for this_group_index = 1 : length(group_names)
     bar(this_group_subjectindices, wm_perf_data(end,this_group_subjectindices), 'FaceColor', color_groups(this_group_index,:)); hold on;
 end
 
-title('AVG WM CBF','interpreter','none', 'FontSize',18)
-ylabel('AVG WM CBF (?)','interpreter','none')
-set(gca,'XTick',1:length(subjects),'xticklabel',subjects,'TickLabelInterpreter','none')
+set(gca,'XTick',1:length(subjects),'xticklabel',subjects, 'FontSize',8,'TickLabelInterpreter','none')
 xtickangle(45)
 set(gcf, 'ToolBar', 'none');
 set(gcf, 'MenuBar', 'none');
+title('AVG WM CBF','interpreter','none', 'FontSize',16)
+ylabel('AVG WM CBF (ml/100g/min)','interpreter','none')
 
 if save_figures
+     MaximizeFigureWindow
     fig_title = 'subjects_WM_CBF';
     filename =  fullfile(project_path, 'figures', fig_title);
     saveas(gca, filename, 'tiff')
 end
 
+
+
+figure; hold on;
+for this_group_index = 1 : length(group_names)
+    this_group_subjectindices = find(group_ids==this_group_index);
+    this_group_and_roi_vol_results = gm_perf_calib_data(end,this_group_subjectindices);
+    
+    singleBoxPlot(this_group_and_roi_vol_results,'abscissa', this_group_index, 'EdgeColor',color_groups(this_group_index,:), 'MarkerColor',color_groups(this_group_index,:),'WiskColor',color_groups(this_group_index,:), 'MeanColor',color_groups(this_group_index,:))
+    
+end
+xlim([ 0 length(this_group_index+1)])
+xlim([0 length(group_names)+1])
+
+ylabel('Perfusion Calib (ml/100g/min)')
+set(gca, 'XTick',1:length(group_names),'xticklabel', group_names,'TickLabelInterpreter','none','FontSize',8)
+%     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+title('AVG GM CALIB CBF','interpreter','latex','FontSize',16)
+xtickangle(45)
+% if no_labels
+%     set(gcf, 'ToolBar', 'none');
+%     set(gcf, 'MenuBar', 'none');
+%     set(get(gca, 'xlabel'), 'visible', 'off');
+%     set(get(gca, 'ylabel'), 'visible', 'off');
+%     set(get(gca, 'title'), 'visible', 'off');
+%     legend(gca, 'hide');
+% end
+if save_figures
+    fig_title = strcat('group_GM_calib_CBF');
+    
+    filename =  fullfile(project_path, 'figures', fig_title);
+    saveas(gca, filename, 'tiff')
+end
 
 figure; hold on;
 for this_group_index = 1 : length(group_names)
@@ -104,14 +206,15 @@ for this_group_index = 1 : length(group_names)
     bar(this_group_subjectindices, gm_perf_calib_data(end,this_group_subjectindices), 'FaceColor', color_groups(this_group_index,:)); hold on;
 end
 
-title('AVG GM CALIB CBF','interpreter','none', 'FontSize',18)
-ylabel('AVG GM CALIB CBF (?)','interpreter','none')
-set(gca,'XTick',1:length(subjects),'xticklabel',subjects,'TickLabelInterpreter','none')
+set(gca,'XTick',1:length(subjects),'xticklabel',subjects, 'FontSize',8,'TickLabelInterpreter','none')
 xtickangle(45)
 set(gcf, 'ToolBar', 'none');
 set(gcf, 'MenuBar', 'none');
+title('AVG GM CALIB CBF','interpreter','none', 'FontSize',16)
+ylabel('AVG GM CALIB CBF (ml/100g/min)','interpreter','none')
 
 if save_figures
+    MaximizeFigureWindow
     fig_title = 'subjects_GM_CALIB_CBF';
     filename =  fullfile(project_path, 'figures', fig_title);
     saveas(gca, filename, 'tiff')
@@ -121,24 +224,54 @@ end
 figure; hold on;
 for this_group_index = 1 : length(group_names)
     this_group_subjectindices = find(group_ids==this_group_index);
+    this_group_and_roi_vol_results = wm_perf_calib_data(end,this_group_subjectindices);
     
-    bar(this_group_subjectindices, wm_perf_calib_data(end,this_group_subjectindices), 'FaceColor', color_groups(this_group_index,:)); hold on;
+    singleBoxPlot(this_group_and_roi_vol_results,'abscissa', this_group_index, 'EdgeColor',color_groups(this_group_index,:), 'MarkerColor',color_groups(this_group_index,:),'WiskColor',color_groups(this_group_index,:), 'MeanColor',color_groups(this_group_index,:))
+    
 end
+xlim([ 0 length(this_group_index+1)])
+xlim([0 length(group_names)+1])
 
-title('AVG WM CALIB CBF','interpreter','none', 'FontSize',18)
-ylabel('AVG WM CALIB CBF (?)','interpreter','none')
-set(gca,'XTick',1:length(subjects),'xticklabel',subjects,'TickLabelInterpreter','none')
+ylabel('Perfusion Calib (ml/100g/min)')
+set(gca, 'XTick',1:length(group_names),'xticklabel', group_names,'TickLabelInterpreter','none','FontSize',8)
+%     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+title('AVG WM CALIB CBF','interpreter','latex','FontSize',16)
 xtickangle(45)
-set(gcf, 'ToolBar', 'none');
-set(gcf, 'MenuBar', 'none');
-
+% if no_labels
+%     set(gcf, 'ToolBar', 'none');
+%     set(gcf, 'MenuBar', 'none');
+%     set(get(gca, 'xlabel'), 'visible', 'off');
+%     set(get(gca, 'ylabel'), 'visible', 'off');
+%     set(get(gca, 'title'), 'visible', 'off');
+%     legend(gca, 'hide');
+% end
 if save_figures
-    fig_title = 'subjects_WM_CALIB_CBF';
+    fig_title = strcat('group_WM_calib_CBF');
+    
     filename =  fullfile(project_path, 'figures', fig_title);
     saveas(gca, filename, 'tiff')
 end
 
+figure; hold on;
+for this_group_index = 1 : length(group_names)
+    this_group_subjectindices = find(group_ids==this_group_index);
+    
+    bar(this_group_subjectindices, wm_perf_calib_data(end,this_group_subjectindices), 'FaceColor', color_groups(this_group_index,:)); hold on;
+end
 
+set(gca,'XTick',1:length(subjects),'xticklabel',subjects, 'FontSize',8,'TickLabelInterpreter','none')
+xtickangle(45)
+set(gcf, 'ToolBar', 'none');
+set(gcf, 'MenuBar', 'none');
+title('AVG WM CALIB CBF','interpreter','none', 'FontSize',16)
+ylabel('AVG WM CALIB CBF (ml/100g/min)','interpreter','none')
+
+if save_figures
+    MaximizeFigureWindow
+    fig_title = 'subjects_WM_CALIB_CBF';
+    filename =  fullfile(project_path, 'figures', fig_title);
+    saveas(gca, filename, 'tiff')
+end
 end
 
 
---
+++ processing_scripts/asl_processing/plot_asl_results.m
@@ -0,0 +1,203 @@
+function plot_asl_results(varargin)
+% plot_CAT12_results('task_folder', '05_MotorImagery', 'subjects',
+% plot_CAT12_results('task_folder', '02_T1', 'subjects', {'1002','1004','1007','1009','1010','1011','1013','1020','1022','1027','1024','2021','2015','2002','2018','2017','2012','2025','2020','2026','2023','2022','2007','2013','2008','2033','2034','2037','2052','2042','3004','3006','3007','3008'},'group_names',{'YA' 'hOA' 'lOA'},'group_ids',[1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3],'save_figures',0, 'no_labels', 0)
+parser = inputParser;
+parser.KeepUnmatched = true;
+% setup defaults in case no arguments specified
+addParameter(parser, 'subjects', '')
+addParameter(parser, 'group_names', '')
+addParameter(parser, 'group_ids', '')
+addParameter(parser, 'no_labels', 0)
+addParameter(parser, 'save_figures', 0)
+addParameter(parser, 'save_scores', 0)
+addParameter(parser, 'results_foldername', '')
+parse(parser, varargin{:})
+subjects = parser.Results.subjects;
+group_names = parser.Results.group_names;
+group_ids = parser.Results.group_ids;
+no_labels = parser.Results.no_labels;
+save_figures = parser.Results.save_figures;
+results_foldername = parser.Results.results_foldername;
+save_scores = parser.Results.save_scores;
+
+% close all;
+project_path = pwd;
+group_color_matrix = distinguishable_colors(length(group_names));
+
+for this_subject_index = 1 : length(subjects)
+    disp(['collecting:' subjects{this_subject_index}])
+    subj_results_dir = fullfile(project_path, subjects{this_subject_index}, 'Processed', 'MRI_files', '07_ASL',results_foldername,'ANTS_Normalization');
+    
+    asl_results_dir = fullfile(subj_results_dir, strcat(subjects{this_subject_index},'_fsl_perfusion.txt'));
+    fileID = fopen(asl_results_dir);
+    
+    data = textscan(fileID,'%s','delimiter',',','headerlines',0);
+    fclose('all');
+    data = reshape(data{:},length(data{1})/2,2);
+    for this_beta = 3:length(data)
+        split_condition_name = strsplit(data{this_beta,1},'_');
+        % WARNING: reading roi names and overwriting (per subject) may be
+        % an issue if different subjs have different roi results files...
+        % need to write a buffer for this..
+        roi_names{this_beta-2} = data{this_beta,1};
+        fa_results(this_subject_index,this_beta-2) = str2num(data{this_beta,2});
+    end
+    
+    asl_results_dir = fullfile(subj_results_dir, strcat(subjects{this_subject_index},'_fsl_perfusion_calib.txt'));
+    fileID = fopen(asl_results_dir);
+    
+    data = textscan(fileID,'%s','delimiter',',','headerlines',0);
+    fclose('all');
+    data = reshape(data{:},length(data{1})/2,2);
+    for this_beta = 3:length(data)
+        split_condition_name = strsplit(data{this_beta,1},'_');
+        % WARNING: reading roi names and overwriting (per subject) may be
+        % an issue if different subjs have different roi results files...
+        % need to write a buffer for this..
+        roi_names{this_beta-2} = data{this_beta,1};
+        fa_calib_results(this_subject_index,this_beta-2) = str2num(data{this_beta,2});
+    end
+end
+
+for this_roi_index = 1: length(roi_names)    
+    figure; hold on;
+    for this_group_index = 1 : length(group_names)
+        this_group_subjectindices = find(group_ids==this_group_index);
+        this_group_and_roi_vol_results = fa_results(this_group_subjectindices,this_roi_index);
+        
+        singleBoxPlot(this_group_and_roi_vol_results,'abscissa', this_group_index, 'EdgeColor',group_color_matrix(this_group_index,:), 'MarkerColor',group_color_matrix(this_group_index,:),'WiskColor',group_color_matrix(this_group_index,:), 'MeanColor',group_color_matrix(this_group_index,:))
+        
+    end
+    xlim([ 0 length(this_group_index+1)])
+    title([roi_names(this_roi_index)],'interpreter','latex')
+    xlim([0 length(group_names)+1])
+    
+    ylabel('Perfusion (ml/100g/min)')
+    set(gca, 'XTick',1:length(group_names),'xticklabel', group_names,'TickLabelInterpreter','none','FontSize',16)
+%     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+    xtickangle(45)
+    if no_labels
+        set(gcf, 'ToolBar', 'none');
+        set(gcf, 'MenuBar', 'none');
+        set(get(gca, 'xlabel'), 'visible', 'off');
+        set(get(gca, 'ylabel'), 'visible', 'off');
+        set(get(gca, 'title'), 'visible', 'off');
+        legend(gca, 'hide');
+    end
+    if save_figures
+        fig_title = strcat('group_',roi_names{this_roi_index},'_asl');
+        
+        filename =  fullfile(project_path, 'figures', fig_title);
+        saveas(gca, filename, 'tiff')
+    end
+    
+    figure; hold on;
+    for this_group_index = 1 : length(group_names)
+        this_group_subjectindices = find(group_ids==this_group_index);
+        this_group_and_roi_vol_results = fa_results(this_group_subjectindices,this_roi_index);
+        
+        bar(this_group_subjectindices, this_group_and_roi_vol_results, 'FaceColor', group_color_matrix(this_group_index,:)); hold on;
+    end
+%     xlim([ 0 length(this_group_index+1)])
+    title([roi_names(this_roi_index)],'interpreter','latex','FontSize',16)
+%     xlim([0 length(group_names)+1])
+    
+    ylabel('Perfusion (ml/100g/min)')
+    set(gca, 'XTick',1:length(subjects),'xticklabel',subjects,'TickLabelInterpreter','none','FontSize',8)
+    title([roi_names(this_roi_index)],'interpreter','latex','FontSize',16)
+%     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+    xtickangle(45)
+    if no_labels
+        set(gcf, 'ToolBar', 'none');
+        set(gcf, 'MenuBar', 'none');
+        set(get(gca, 'xlabel'), 'visible', 'off');
+        set(get(gca, 'ylabel'), 'visible', 'off');
+        set(get(gca, 'title'), 'visible', 'off');
+        legend(gca, 'hide');
+    end
+    if save_figures
+        MaximizeFigureWindow
+        fig_title = strcat('subject_',roi_names{this_roi_index},'_asl');
+        filename =  fullfile(project_path, 'figures', fig_title);
+        saveas(gca, filename, 'tiff')
+    end
+    
+end
+
+for this_roi_index = 1: length(roi_names)    
+    figure; hold on;
+    for this_group_index = 1 : length(group_names)
+        this_group_subjectindices = find(group_ids==this_group_index);
+        this_group_and_roi_vol_results = fa_calib_results(this_group_subjectindices,this_roi_index);
+        
+        singleBoxPlot(this_group_and_roi_vol_results,'abscissa', this_group_index, 'EdgeColor',group_color_matrix(this_group_index,:), 'MarkerColor',group_color_matrix(this_group_index,:),'WiskColor',group_color_matrix(this_group_index,:), 'MeanColor',group_color_matrix(this_group_index,:))
+        
+    end
+    xlim([ 0 length(this_group_index+1)])
+    xlim([0 length(group_names)+1])
+    
+    ylabel('Perfusion Calib (ml/100g/min)')
+    set(gca, 'XTick',1:length(group_names),'xticklabel', group_names,'TickLabelInterpreter','none','FontSize',8)
+%     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+    title([roi_names(this_roi_index)],'interpreter','latex','FontSize',16)
+    xtickangle(45)
+    if no_labels
+        set(gcf, 'ToolBar', 'none');
+        set(gcf, 'MenuBar', 'none');
+        set(get(gca, 'xlabel'), 'visible', 'off');
+        set(get(gca, 'ylabel'), 'visible', 'off');
+        set(get(gca, 'title'), 'visible', 'off');
+        legend(gca, 'hide');
+    end
+    if save_figures
+        fig_title = strcat('group_',roi_names{this_roi_index},'_asl_calib');
+        
+        filename =  fullfile(project_path, 'figures', fig_title);
+        saveas(gca, filename, 'tiff')
+    end
+    
+    
+    figure; hold on;
+    for this_group_index = 1 : length(group_names)
+        this_group_subjectindices = find(group_ids==this_group_index);
+        this_group_and_roi_vol_results = fa_calib_results(this_group_subjectindices,this_roi_index);
+        
+        bar(this_group_subjectindices, this_group_and_roi_vol_results, 'FaceColor', group_color_matrix(this_group_index,:)); hold on;
+    end
+%     xlim([ 0 length(this_group_index+1)])
+    
+%     xlim([0 length(group_names)+1])
+    
+    ylabel('Perfusion Calib (ml/100g/min)')
+    set(gca, 'XTick',1:length(subjects),'xticklabel',subjects,'TickLabelInterpreter','none','FontSize',8)
+    title([roi_names(this_roi_index)],'interpreter','latex','FontSize',16)
+    %     set(gca,'XTick',1:length(group_names),'xticklabel',{'YA','high-OA','low-OA'},'TickLabelInterpreter','none','FontSize',16)
+    xtickangle(45)
+    if no_labels
+        set(gcf, 'ToolBar', 'none');
+        set(gcf, 'MenuBar', 'none');
+        set(get(gca, 'xlabel'), 'visible', 'off');
+        set(get(gca, 'ylabel'), 'visible', 'off');
+        set(get(gca, 'title'), 'visible', 'off');
+        legend(gca, 'hide');
+    end
+    if save_figures
+        MaximizeFigureWindow
+        fig_title = strcat('subject_',roi_names{this_roi_index},'_asl_calib');
+        
+        filename =  fullfile(project_path, 'figures', fig_title);
+        saveas(gca, filename, 'tiff')
+    end
+end
+
+
+% if save_scores
+%     subject_table = array2table(subjects');
+%     subject_table.Properties.VariableNames = {'subject_ids'};
+%     
+%     fa_cell_table = array2table(fa_results);
+%     fa_cell_table.Properties.VariableNames = roi_names;
+%     fa_results_table = [subject_table, fa_cell_table];
+%     writetable(fa_results_table,fullfile(project_path,'spreadsheet_data','asl_intensity_score.csv'))
+% end
+end
