8cd35a433e38820039c956a1b14cd04b0d5bb1d9
tfettrow <tfettrow@gmail.com>
2021-12-26 12:16:53 -0500

    asl

---
+++ processing_scripts/asl_processing/realign_asl.m
@@ -0,0 +1,41 @@
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+function realign_asl(asl_file_name)
+data_path = pwd;
+
+clear matlabbatch
+spm('Defaults','fMRI');
+spm_jobman('initcfg');
+spm_get_defaults('cmdline',true);
+
+this_file_to_realign = spm_select('FPList', data_path, strcat('^',asl_file_name,'$'));
+
+
+this_file_with_volumes = spm_select('expand', this_file_to_realign);
+
+matlabbatch{1}.spm.spatial.realign.estwrite.data = {cellstr(this_file_with_volumes)};
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [2 1];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.prefix = 'realigned_';
+spm_jobman('run',matlabbatch);
+clear matlabbatch
+end
