0bb871d1d99edb339f9336cc50a1c0fce7f22f8a
valay93 <69319816+valay93@users.noreply.github.com>
2022-03-31 10:40:34 -0400

    new files for saving pngs

---
+++ processing_scripts/fmri_processing/save_slicetimedRun_png.m
@@ -0,0 +1,19 @@
+clear, clc
+data_path = pwd;
+b = regexp(data_path, '\d*0*\d','match');
+clear matlabbatch
+spm('Defaults','fMRI');
+spm_jobman('initcfg');
+spm_get_defaults('cmdline',true);
+
+file_names=spm_select('FPList', data_path, '^meanunwarped.*\.nii$');
+
+for i_file = 1 : size(file_names,1)
+    matlabbatch{1}.spm.util.disp.data = {[file_names(i_file,:)]};
+    
+    spm_jobman('run',matlabbatch);
+    
+    saveas(gcf,[b{1} '_slicetimed_run' num2str(i_file)],'png')
+    
+    clear matlabbatch
+end
